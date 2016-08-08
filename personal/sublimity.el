54274c00fbeaac2ebf26916ee85622477e3fe29e
Sohum Banerjea <sohumb@cakesolutions.net>
2016-08-08 18:00:17 +0100

    we can also kill sublimity, I think

--- personal/sublimity.el
+++
@@ -1,8 +0,0 @@
-(prelude-require-package 'sublimity)
-(require 'sublimity)
-(require 'sublimity-scroll)
-;;(require 'sublimity-attractive)
-
-;;(sublimity-map-set-delay 0)
-
-(sublimity-global-mode)
