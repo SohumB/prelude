fb614c263051bb88861fc307b0b357e917f6581b
Sohum Banerjea <sohumb@gmail.com>
2016-11-04 13:27:08 -0700

    replace most of web-mode config with smartparens-html, tho I'm not sure I like it

--- personal/web-mode.el
+++ personal/web-mode.el
@@ -2,15 +2,12 @@
 
 (add-to-list 'auto-mode-alist '("\\.hbs\\'" . web-mode))
 
-(defun my-web-mode-hook ()
-  (setq web-mode-enable-auto-pairing nil))
+(add-hook 'web-mode-hook 'smartparens-html)
 
-(add-hook 'web-mode-hook 'my-web-mode-hook)
+;; (defun sp-web-mode-is-code-context (id action context)
+;;   (when (and (eq action 'insert)
+;;              (not (or (get-text-property (point) 'part-side)
+;;                       (get-text-property (point) 'block-side))))
+;;     t))
 
-(defun sp-web-mode-is-code-context (id action context)
-  (when (and (eq action 'insert)
-             (not (or (get-text-property (point) 'part-side)
-                      (get-text-property (point) 'block-side))))
-    t))
-
-(sp-local-pair 'web-mode "<" nil :when '(sp-web-mode-is-code-context))
+;; (sp-local-pair 'web-mode "<" nil :when '(sp-web-mode-is-code-context))
