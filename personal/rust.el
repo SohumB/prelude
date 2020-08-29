ec6ab12d85d13d3fd5fae5b6f6f79d0627f96828
Sohum Banerjea <sohumb@gmail.com>
2020-08-28 21:19:06 -0700

    enable rust-analyzer

--- personal/rust.el
+++ personal/rust.el
@@ -1,4 +1,4 @@
-(prelude-require-packages '(rust-mode cargo racer flycheck-rust))
+(prelude-require-packages '(rust-mode cargo racer flycheck-rust lsp-mode lsp-ui))
 
 (add-hook 'rust-mode-hook #'racer-mode)
 (add-hook 'rust-mode-hook 'cargo-minor-mode)
@@ -6,8 +6,11 @@
 (add-hook 'racer-mode-hook #'company-mode)
 
 (require 'rust-mode)
-(define-key rust-mode-map (kbd "TAB") #'company-indent-or-complete-common)
+(require 'lsp-mode)
+; (define-key rust-mode-map (kbd "TAB") #'company-indent-or-complete-common)
 (setq company-tooltip-align-annotations t)
+(setq lsp-rust-server 'rust-analyzer)
+(add-hook 'rust-mode-hook #'lsp)
 
 (with-eval-after-load 'rust-mode
   (add-hook 'flycheck-mode-hook #'flycheck-rust-setup))
