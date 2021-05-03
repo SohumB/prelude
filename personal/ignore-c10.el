ddd2f295d16cb363850e8239f3263760ab548505
Sohum Banerjea <sohumb@gmail.com>
2021-05-03 11:23:08 -0700

    ignore mouse-10

--- personal/ignore-c10.el
+++
@@ -1,2 +0,0 @@
-(global-set-key (kbd "<C-mouse-10>") 'ignore)
-(global-set-key (kbd "<C-drag-mouse-10>") 'ignore)
---
+++ personal/ignore-m10.el
@@ -0,0 +1,3 @@
+(define-key special-event-map (kbd "<C-mouse-10>") 'ignore)
+(define-key special-event-map (kbd "<C-drag-mouse-10>") 'ignore)
+(define-key special-event-map (kbd "<mouse-10>") 'ignore)
